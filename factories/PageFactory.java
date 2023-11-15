3b2b7fefc274c582b129913afca6bca8e0fa9a05
Eslam <11410120210064@stud.cu.edu.eg>
2023-11-15 19:51:42 +0200

    added factories documentation

--- factories/PageFactory.java
+++ factories/PageFactory.java
@@ -2,6 +2,7 @@ package factories;
 
 import java.util.LinkedList;
 import java.util.List;
+
 import controllers.BillController;
 import controllers.MoneyController;
 import controllers.UserController;
@@ -16,60 +17,80 @@ import views.PayBillPage;
 import views.RegisterationPage;
 import views.TransferMoneyPage;
 
+/**
+ * Factory class for creating different pages based on user input.
+ * Manages the creation of pages and options based on the provided parameters.
+ */
 public class PageFactory {
 
-    boolean isValidToekn(String token) {
-        if (token == null)
-            return false;
-        return true;
+    /**
+     * Checks if the provided token is valid.
+     *
+     * @param token The token to be checked for validity.
+     * @return True if the token is not null, otherwise false.
+     */
+    boolean isValidToken(String token) {
+        return token != null;
     }
 
+    /**
+     * Creates a page based on the given page name and token.
+     *
+     * @param pageName The name of the page to be created.
+     * @param token    The user token for authentication.
+     * @return The created page instance.
+     */
     public Page createPage(String pageName, String token) {
 
-        if (pageName.equals("exit"))
+        if ("exit".equals(pageName))
             return null;
 
         DataBaseRepo db = DataBaseV1.getDB();
 
-        if (isValidToekn(token)) {
-            if (pageName.equals("balance"))
+        if (isValidToken(token)) {
+            if ("balance".equals(pageName))
                 return new BalancePage(new MoneyController(db));
 
-            if (pageName.equals("bills"))
+            if ("bills".equals(pageName))
                 return new PayBillPage(new BillController(db), new BillController(db));
 
-            if (pageName.equals("money"))
+            if ("money".equals(pageName))
                 return new TransferMoneyPage(new MoneyController(db));
 
-            if (pageName.equals("logout"))
+            if ("logout".equals(pageName))
                 return new LogoutPage();
 
         } else {
-            if (pageName.equals("signup"))
+            if ("signup".equals(pageName))
                 return new RegisterationPage(new UserController(db));
 
-            if (pageName.equals("login"))
+            if ("login".equals(pageName))
                 return new LoginPage(new UserController(db));
         }
 
         return new NotFoundPage();
     }
 
-    public List<String> createOptions(String Token) {
-        List<String> options = new LinkedList<String>();
+    /**
+     * Creates a list of options based on the provided token.
+     *
+     * @param token The user token for authentication.
+     * @return A list of options available to the user.
+     */
+    public List<String> createOptions(String token) {
+        List<String> options = new LinkedList<>();
 
-        if (isValidToekn(Token)) {
+        if (isValidToken(token)) {
             options.add("(balance) show your balance");
-            options.add("(bills) pay bills your balance");
-            options.add("(money) transfer money  your balance");
+            options.add("(bills) pay bills from your balance");
+            options.add("(money) transfer money from your balance");
             options.add("(logout) logout from this account");
         } else {
-            options.add("(signup) create new account");
+            options.add("(signup) create a new account");
             options.add("(login) login with your account");
         }
 
-        options.add("(exit) exit from application");
+        options.add("(exit) exit from the application");
         return options;
     }
-
 }
--- factories/ProviderFactory.java
+++ factories/ProviderFactory.java
@@ -5,8 +5,18 @@ import classes.MoneyProvider;
 import classes.WalletProvider;
 import models.UserProvider;
 
+/**
+ * Factory class for creating MoneyProvider instances based on the account type of a UserProvider.
+ * Supports creating BankProvider and WalletProvider instances.
+ */
 public class ProviderFactory {
 
+    /**
+     * Creates a MoneyProvider instance based on the account type of the provided UserProvider.
+     *
+     * @param srcProvider The UserProvider instance for which the MoneyProvider is created.
+     * @return A MoneyProvider instance corresponding to the account type of the UserProvider.
+     */
     public static MoneyProvider create(UserProvider srcProvider) {
 
         switch (srcProvider.accountType) {
